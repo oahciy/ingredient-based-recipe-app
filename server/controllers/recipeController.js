ca1aee146529158835a6bcd43f1a12a91407aa39
Yichao <yq2550@gmail.com>
2022-11-02 14:22:53 +0000

    client end: add link to recipe name; server end: recipe id can be accessed by req.params

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -10,6 +10,15 @@ const RecipesController = {
     console.log(data);
     res.json(data);
   },
+
+  FindByid: async (req, res) => {
+    const { id } = req.params;
+    const url = `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    res.json(data);
+  }
 };
 
 module.exports = RecipesController;
