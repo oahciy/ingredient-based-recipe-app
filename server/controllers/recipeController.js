d7d1a78f6dff9d3e56a9ee02cd1ebefdbe5daae1
postie89 <t.mannion89@gmail.com>
2022-11-02 10:46:53 +0000

    working first draft

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,15 +1,4 @@
-const asyncHandler = require("express-async-handler");
 const fetch = require("node-fetch");
-
-// GET from cocktail API
-// const getRecipes = asyncHandler(async (req, res) => {
-//   //const { search } = req.query;
-//   console.log("getRecipes");
-//   const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin`;
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   res.json(data);
-// });
 
 const RecipesController = {
   All: async (req, res) => {
