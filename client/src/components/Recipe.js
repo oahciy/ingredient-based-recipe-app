84a8a5f71635da1bf5f2eb45f13abc6f7f2d26a8
Marina Ivanova <ubcg62bs@Marinas-MacBook-Pro.local>
2022-11-03 12:09:58 +0000

    Recipe individual card

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -5,53 +5,83 @@ import axios from "axios";
 // import { Link } from "react-router-dom";
 
 function Recipe() {
-  const [recipe, setRecipe] = useState([]);
+  const [drink, setRecipe] = useState([]);
   const [content, setContent] = useState([]);
+
+  // const getIngredients = () => {
+  //   let ingredientsArray = [];
+  //   for (let i = 0; i < 15; i++) {
+  //     // loop over the ingredients in recipe.drink[0]
+  //     if (drink[`strIngredient${i}`] !== null) {
+  //       ingredientsArray.push(
+  //         drink[`strIngredient${i}`] +
+  //           " " +
+  //           drink[`strMeasure${i}`]
+  //       );
+  //     }
+  //   }
+  //   console.log("ingredients: " + ingredientsArray);
+  //   setContent(ingredientsArray);
+  // };
+
+  const getRecipe = async () => {
+    // console.log("start get recipe");
+    const id = window.location.href.split("/").reverse()[0];
+    const response = await axios.get(`http://localhost:9000/recipe/${id}`);
+    const drink = response.data.drinks[0]
+    setRecipe(drink);
+
+    let ingredientsArray = [];
+    for (let i = 1; i <= 15; i++) {
+      // console.log(drink[`strIngredient${i}`])
+      // loop over the ingredients in recipe.drink[0]
+      if (drink[`strIngredient${i}`] !== null) {
+        ingredientsArray.push(
+          drink[`strIngredient${i}`] +
+            " " +
+            drink[`strMeasure${i}`]
+        );
+      }
+    }
+    // console.log("ingredients: " + ingredientsArray);
+    setContent(ingredientsArray);
+    
+    
+    // console.log("end");
+  };
 
   useEffect(() => {
     getRecipe();
-  });
+  }, []);
 
-  const getRecipe = async () => {
-    console.log("start get recipe");
-    const id = window.location.href.split("/").reverse()[0];
-    const response = await axios.get(`http://localhost:9000/recipe/${id}`);
-    setRecipe(response.data);
-    console.log("end");
-  };
 
-  const getIngredients = () => {
-    let ingredientsArray = [];
-    for (let i = 0; i < 15; i++) {
-      let strValue = "strIngredient" + i;
-      console.log(strValue + "1");
-      console.log(recipe);
-      console.log(recipe.drinks[strValue] + "3");
-      ingredientsArray.push(recipe.drinks[strValue]);
-    }
 
-    setContent(ingredientsArray.filter((ingredient) => ingredient !== null));
-  };
 
+
+  // const getIngredients = () => {
+  //   let ingredientsArray = [];
+  //   for (let i = 0; i < 15; i++) {
+  //     let strValue = "strIngredient" + i;
+  //     console.log(strValue + "1");
+  //     console.log(recipe);
+  //     console.log(recipe.drinks[strValue] + "3");
+  //     ingredientsArray.push(recipe.drinks[strValue]);
+  //   }
+
+  //   setContent(ingredientsArray.filter((ingredient) => ingredient !== null));
+  // };
   return (
     <div>
-      <button onClick={getIngredients}>Get Ingredients</button>
-      {console.log(recipe)}
-      {console.log(recipe.drinks)}
-      {console.log(recipe.drinks[0])}
-      {/* {recipe.drinks?.map((recipe) => (
-        <div> */}
-          <h1>{recipe.drinks.strDrink}</h1>
-          <p>{recipe.drinks.strInstructions}</p>
-          <img src={recipe.drinks.strDrinkThumb} alt={recipe.drinks.strDrink} />
-          <ul>
-            {console.log(content)}
-            {/* {content.map((ingredient) => (
-              <div key={ingredient}>
-                <li>{ingredient}</li>
-              </div>
-            ))} */}
-          </ul>
+      {console.log(drink)}
+      {console.log(content)}
+      <div><img src={drink.strDrinkThumb}></img></div>
+      <div><h3>{drink.strDrink}</h3></div>
+      {content.map((ingredient) => (
+        <div key={ingredient}>
+          <li>{ingredient}</li>
+        </div>
+      ))}
+      <div>{drink.strInstructions}</div>
     </div>
   );
 }
